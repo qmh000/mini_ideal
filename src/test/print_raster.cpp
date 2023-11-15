6dfe504afa907a417c9d44fb7d8e18c2fbc7d4e2
qmh000 <qiminhan668@163.com>
2023-11-15 09:21:13 +0800

    update 2023/11/15

--- src/test/print_raster.cpp
+++ src/test/print_raster.cpp
@@ -4,16 +4,25 @@
 
 int main(){
 	query_context ctx;
-	vector<MyPolygon *> source = load_binary_file("/home/qmh/mini_ideal/src/has_child.idl",ctx);
+	// vector<MyPolygon *> source = load_binary_file("/home/qmh/mini_ideal/src/has_child.idl",ctx);
 
-	for(MyPolygon *p:source){
-		if(p->boundary->num_vertices>10000){
-			p->rasterization(100);
-			p->print();
-			p->get_rastor()->print();
-			return 0;
-		}
-	}
+	// for(MyPolygon *p:source){
+	// 	if(p->boundary->num_vertices>10000){
+	// 		p->rasterization(100);
+	// 		// p->print();
+	// 		p->get_rastor()->print();
+	// 		sleep(1000);
+	// 		return 0;
+	// 	}
+	// }
+
+	
+	MyPolygon *source = load_binary_file_single("/home/qmh/mini_ideal/src/has_child.idl",ctx,0);
+
+	cout << source->boundary->num_vertices << endl;
+
+	source->rasterization(100);
+	source->get_rastor()->print();
 
 	return 0;
 }
